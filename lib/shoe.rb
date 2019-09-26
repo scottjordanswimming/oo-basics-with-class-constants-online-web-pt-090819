d5e6baed4eaf46c6059f00b4ef20ea432620e2fd
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-26 21:29:09 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
-  BRAND = []
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
@@ -12,27 +12,10 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
 
 end
-
-
-class Book
-  attr_accessor :author, :page_count  # remove the attr_accessor for genre
-  attr_reader :title, :genre  # add an attr_reader for genre
- 
-  GENRES = []
- 
-  def initialize(title)
-    @title = title
-  end
- 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
- 
-  # create the writer for genre and add the logic for the class constant
-  def genre=(genre)
-    @genre = genre
-    GENRES << genre 
-  end
 end
